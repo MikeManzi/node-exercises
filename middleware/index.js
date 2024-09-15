8f989ed984a32aa14b6b03045e5f460a9b0d1870
MikeManzi <manzimike378@gmail.com>
2024-09-15 15:13:29 +0200

    chore: add middleware to log the time

--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,17 @@ const express = require("express")
 
 const app = express()
 
+const middleware = (req, res, next) => {
+    console.log(`Time of the request: ${new Date()}`)
+    next()
+}
+
+app.use("/api", middleware)
+
+app.get('/api/home', (req, res) => {
+    res.send("Hello World")
+})
+
 const PORT = 3000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
